f8d5aab0426b4bd983a9c1feceb89f8e6ba7558b
Matue777x666 <matheusemail222@gmail.com>
2025-05-14 09:50:12 -0300

    update

--- script.js
+++ script.js
@@ -3,40 +3,52 @@ const lampadaOf = document.querySelector('#lampadaOf')
 const liga = document.querySelector('#liga')
 const desliga = document.querySelector('#desliga')
 const lampadaQuebra = document.querySelector('#lampadaQuebra')
+const troca = document.querySelector('#troca')
 
 liga.addEventListener("click" ,acender)
 desliga.addEventListener("click",apagar)
 lampadaOf.addEventListener("dblclick",quebrar)
 lampadaOn.addEventListener("dblclick",quebrar)
+troca.addEventListener('click',trocar)
 
 let quebrada = false
 
-function acender(){ 
+function acender(){
+    lampadaOf.style.display ='none'
+    lampadaOn.style.display ='block'
     if(quebrada){
-        alert('Não é possivel acender a lampada quebrada')
-        return;
+        alert('A lampada está quebrada!')
+         lampadaOf.style.display ='none'
+         lampadaOn.style.display ='none'
     }
-    lampadaOn.style.display = 'block'
-    lampadaOf.style.display = 'none'
 
 }
 
 function apagar(){
+    lampadaOf.style.display ='block'
+    lampadaOn.style.display ='none'
     if(quebrada){
-        alert('Não tente desligar,a lampada quebrada')
-        return;
+        alert('A lampada está quebrada!')
+         lampadaOf.style.display ='none'
+         lampadaOn.style.display ='none'
     }
-    lampadaOn.style.display = 'none'
-    lampadaOf.style.display = 'block'
+
+
 }
 
 function quebrar(){
-     lampadaOn.style.display = 'none'
-     lampadaOf.style.display = 'none'
-     lampadaQuebra.style.display='block'
      quebrada = true
-     alert('A lampada quebrou!!')
-     
+     lampadaOf.style.display ='none'
+     lampadaOn.style.display ='none'
+     lampadaQuebra.style.display ='block'
+     troca.style.display = 'block'
+
 }
 
-
+function trocar(){
+     quebrada = false
+     troca.style.display = 'none'
+     lampadaOf.style.display ='block'
+     lampadaOn.style.display ='none'
+     lampadaQuebra.style.display ='none'
+}
